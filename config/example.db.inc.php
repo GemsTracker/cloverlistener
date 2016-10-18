a05a50564ea9c7935d0f7a702b09a9fbcdba06b3
Matijs de Jong <mjong@magnafacta.nl>
2016-10-18 12:48:00 +0200

    Started on library implementation

---
+++ config/example.db.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+define ('HOST', 'localhost');
+define ('USER', 'testuser');
+define ('PASSWD', 'testpwd');
+define ('DATABASE', 'testdb');
+
+//-------------------------------------------------------------------------------------------------
+// Optional environmental settings for your project
+//-------------------------------------------------------------------------------------------------
+defined('APPLICATION_ENV') || define('APPLICATION_ENV',  'development');
